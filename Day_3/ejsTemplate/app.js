674d8b86c7eaff4718d1a9022ad5e0e31101577d
umang <umangdhandhukiya97@gmail.com>
2025-06-19 11:46:36 +0530

    use ejs and add static file

--- Day_3/ejsTemplate/app.js
+++ Day_3/ejsTemplate/app.js
@@ -6,12 +6,19 @@ app.use(express.json()); //for convert data into json
 
 app.set("view engine", "ejs"); //to set ejs for define we are using ejs
 app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname,"public")))
 
 // ejs = embeded javaScript which is used for html and js combined
 // set is used for declare we are using ejs in our file
 
 app.get('/',(req,res)=>{
-    res.render("index")
+    let number = Math.floor(Math.random()*10)
+    res.render("index",{number})
+})
+
+app.get('/todo',(req,res)=>{
+  let myTask = ["karate","coding","playing","walking"]
+  res.render("todo",{myTask})
 })
 
 app.listen(PORT, (req, res) => {
